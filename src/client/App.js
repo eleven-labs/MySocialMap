4119c625d338f517ad56275aabd435a2d02ec449
Jonathan Jalouzot <jjalouzot@groupe-exp.com>
2014-08-08 16:08:22 +0200

    Add io socket for real time

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,11 @@
 'use strict';
 
-angular.module('socialMapApp',['google-maps', 'ngRoute', 'angularFileUpload', 'ngAutocomplete'])
+angular.module('socialMapApp',['google-maps', 'ngRoute', 'angularFileUpload', 'ngAutocomplete', 'btford.socket-io'])
+    .factory('socket', function (socketFactory) {
+        return socketFactory({
+            ioSocket: io.connect()
+        })
+    })
     .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
         $routeProvider.
             when('/', {
